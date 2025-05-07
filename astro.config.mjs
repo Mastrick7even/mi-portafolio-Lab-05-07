e1da53f9fabd970eb8fe4430b95e3ecfddbcd378
Bastian-dev <b.arriagada2201@alumnos.ubiobio.cl>
2025-05-07 13:34:38 -0400

    Commit inicial portafolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    base: '/mi-portafolio-Lab-05-07/', //!mismo nombre que el repo de github
+    output: 'static', //!para que el sitio sea esttico
+});
